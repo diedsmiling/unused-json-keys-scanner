45f7973ecbb5f165e6ffee3f9d1fcdd5127be155
Alexandr Lazarev <alexandr_lazarev@bonify.de>
2017-12-18 12:06:18 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"errors"
+	"fmt"
+)
+
+func parseArgs() (string, error) {
+	dir := flag.String("dir", "", "Path to directory that should be scanned")
+	flag.Parse()
+	if *dir == "" {
+		return "", errors.New("no directory specified")
+	}
+	return *dir, nil
+}
+
+func (d dir) scan() bool {
+	fmt.Printf("Scan %s", d.name)
+	return true
+}
+
+func main() {
+	directory, err := parseArgs()
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	var rootDir = dir{name: directory}
+	rootDir.scan()
+
+
+}
